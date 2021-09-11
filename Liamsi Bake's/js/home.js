8a3be9adb2cdf1a871680210b2cdb42b79256b70
ismail224 <85870873+ismail224@users.noreply.github.com>
2021-09-11 13:52:18 +0530

    TASK-1

---
+++ Liamsi Bake's/js/home.js
@@ -0,0 +1,35 @@
+let flag = 0;
+
+function controller(x){
+    flag = flag+x;
+
+    //left x=-1
+    //flag=2+(-1)=1
+    //right x=1
+    //flag=2+(1)=3
+    slideshow(flag);
+}
+
+slideshow(flag);
+
+function slideshow(num){
+    let slides = document.getElementsByClassName('slide');
+
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }
+
+    if(num < 0){
+        flag=slides.length-1;
+        num=slides.length-1;
+    }
+
+    for(let y of slides){
+        y.style.display ="none";
+
+    }
+
+    slides[num].style.display = "block";
+    
+}
